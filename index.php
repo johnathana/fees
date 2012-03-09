6691a3aa1eb2809e7aa22846a27b7372ca55a05e
JohnAthana <johnathana@gmail.com>
2012-03-09 15:22:58 +0200

    The auth class implements the CAS authentication method

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <!DOCTYPE html> 
-<?php
-include_once('includes/CAS.php');
 
-phpCAS::setDebug(false);
-phpCAS::client(SAML_VERSION_1_1,'login.uoa.gr',443,'');
-phpCAS::setNoCasServerValidation();
-phpCAS::forceAuthentication();
-if (isset($_REQUEST['logout'])) {
-       phpCAS::logout();
-}
-
-
-
-$user=phpCAS::getUser();
-$attr=phpCAS::getAttributes();
-
-
-?>
 <html> 
 <head>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/head.php'); ?>
