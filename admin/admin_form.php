c9272d1de2046c9af9fd154cf60cedb7899ebf2b
JohnAthana <johnathana@gmail.com>
2012-07-03 10:46:42 +0300

    Admin heavy patching

--- admin/admin_form.php
+++ admin/admin_form.php
@@ -68,63 +68,19 @@
 				alert("Πρέπει πρώτα να επιλέξετε έναν φοιτητή για την ανάθεση");
 				return false;
 			}
-		});		
+		});
     });
+
+
 	function radio_click()
 	{
-		
-		if($('input[name=filter1]:checked').val() == "one")
-		{
-			if($('input[name=filter2]:checked').val() == "current")
-			{
-				if($('input[name=filter3]:checked').val() == "live")
-				{
-					window.location.href="admin_form.php?choice=111";
-				}
-				else
-				{
-					window.location.href="admin_form.php?choice=112";
-				}
-			}
-			else
-			{
-				if($('input[name=filter3]:checked').val() == "live")
-				{
-					window.location.href="admin_form.php?choice=121";
-				}
-				else
-				{
-					window.location.href="admin_form.php?choice=122";
-				}
-			}
-		}
-		else
-		{
-			if($('input[name=filter2]:checked').val() == "current")
-			{
-				if($('input[name=filter3]:checked').val() == "live")
-				{
-					window.location.href="admin_form.php?choice=211";
-				}
-				else
-				{
-					window.location.href="admin_form.php?choice=212";
-				}
-			}
-			else
-			{
-				if($('input[name=filter3]:checked').val() == "live")
-				{
-					window.location.href="admin_form.php?choice=221";
-				}
-				else
-				{
-					window.location.href="admin_form.php?choice=222";
-				}
-			}
-		}	 
+		var pending = ($('input[name=filter1]:checked').val() == "one") ? 1 : 0;
+		var current = ($('input[name=filter3]:checked').val() == "live") ? 1 : 0;
+
+		window.location.href = "admin_form.php?current="+current+"&pending="+pending;
 	}
-	
+
+
 	/* Get the rows which are currently selected */
 	function fnGetSelected( oTableLocal )
 	{
@@ -157,99 +113,11 @@
 	<?php 
 		if (isset($_POST['id']))//to id ths workoffer
 		{
-		$work_id = $_POST['id'];
-		$query = "UPDATE work_offers SET published = '1' WHERE id='$work_id'";
-		$result_set = mysql_query($query,$con);
-		confirm_query($result_set);
-	?>
-		<div id="container">
-			<div class="full_width big">
-				<h2>Φόρμα Έγκρισης Παροχών</h2> 
-				<br />
-			</div>
-			
-			<form id="myForm"  method="POST" >
-				<div id="demo" ></div>
-				<div class="demo_jui" id="demo_jui">
-				
-				<?php   
-				if (isset($_GET['choice']))
-				{  
-					$choice = $_GET['choice'];
-					$result = get_admin_data($choice);
-				}
-				else
-				{
-					$choice = "111";//proswpikes-trexon etos-energes
-					$result = get_admin_data($choice);
-				}
-				?>
-				<table style="width: 300px">
-				<tr>
-					<td><label>Εγκεκριμένες παροχές</td>
-					<td><input type="radio" name="filter1" onClick="radio_click();" value="one" 
-					<?php if($choice=="111"||$choice=="112"||$choice=="121"||$choice=="122"){echo "checked=\"true\"";}?> /></label></td>
-					<td><label>Προς έγκριση παροχές</td>
-					<td><input type="radio" name="filter1" onClick="radio_click();" value="all"
-					<?php if($choice=="211"||$choice=="212"||$choice=="221"||$choice=="222"){echo "checked=\"true\"";}?> /></label></td>
-				</tr>
-				<tr>
-					<td><label>Τρέχοντος έτους</td>
-					<td><input type="radio" name="filter2" onClick="radio_click();" value="current"
-					<?php if($choice=="111"||$choice=="112"||$choice=="211"||$choice=="212"){echo "checked=\"true\"";}?> /></label></td>
-					<td><label>Παλιότερων ετών</td>
-					<td><input type="radio" name="filter2" onClick="radio_click();" value="old"
-					<?php if($choice=="121"||$choice=="122"||$choice=="221"||$choice=="222"){echo "checked=\"true\"";}?>/></label></td>
-				</tr>
-				<tr>
-					<td><label>Ενεργές παροχές</td>
-					<td><input type="radio" name="filter3" onClick="radio_click();" value="live"
-					<?php if($choice=="111"||$choice=="121"||$choice=="211"||$choice=="221"){echo "checked=\"true\"";}?> /></label></td>
-					<td><label>Ανενεργές παροχές</td>
-					<td><input type="radio" name="filter3" onClick="radio_click();" value="dead"
-					<?php if($choice=="112"||$choice=="122"||$choice=="212"||$choice=="222"){echo "checked=\"true\"";}?>/></label></td>
-				</tr>
-				</table>
-				<br />	
-				<table cellpadding="0" cellspacing="0" border="0" class="display" id="example" >
-				<thead>
-					<tr>
-						<th>ID παροχής</th>
-						<th>Καθηγητής</th>
-						<th>Τίτλος παροχής</th>
-						<th>Αριθμός υποψηφίων</th>
-						<th>Απαιτήσεις γνώσεων</th>
-						<th>Παραδοτέα </th>
-						<th>Απαιτούμενες ώρες υλοποίησης</th>
-						<th>Λήξη προθεσμίας</th>
-						<th>Ακαδημαϊκό έτος</th>
-						<th>Χειμερινού εξαμήνου</th>
-					</tr>
-				</thead>
-				<tbody>	
-				<?php	while($row = mysql_fetch_assoc($result))
-						{
-							extract($row);
-							echo "<tr>\n";
-							$ayear_row = get_ayear_from_academic_year_id($academic_year_id);
-							echo "<td>$id</td><td>$professor_name</td><td>$title</td><td>$candidates</td><td>$requirements</td><td>$deliverables</td><td>$hours</td><td>$deadline</td><td>$ayear_row[ayear]</td>";
-							echo "<td><input type='checkbox' disabled='true' " . (($winter_semester == 1) ? "checked='true'" : "") . ">";
-							echo "</td></tr>\n";
-						}			
-				?>	
-				</tbody>
-				</table>
-				
-				<br />
-				<p>
-				<?php if($choice=="211"){?><input class="button" type="submit" name="submit_btn" value="Έγκριση παροχής"  /><?php	}?>
-				</p>
-				</div>
-			</form>		
-		</div>
-	<?php
+			$work_id = $_POST['id'];
+			$query = "UPDATE work_offers SET published = '1' WHERE id='$work_id'";
+			$result_set = mysql_query($query,$con);
+			confirm_query($result_set);
 		}
-		else{
 	?>
 		<div id="container">
 			<div class="full_width big">
@@ -262,41 +130,28 @@
 				<div class="demo_jui" id="demo_jui">
 				
 				<?php   
-				if (isset($_GET['choice']))
-				{  
-					$choice = $_GET['choice'];
-					$result = get_admin_data($choice);
-				}
-				else
-				{
-					$choice = "111";//proswpikes-trexon etos-energes
-					$result = get_admin_data($choice);
-				}
+					if (isset($_GET['pending']) && isset($_GET['current'])) {  
+						$pending = $_GET['pending'];
+						$current = $_GET['current'];
+					} else {
+						$pending = 1;
+                                                $current = 1;
+					}
+
+                                        $result = get_workoffer_list($auth->mail, $personal, $current);
 				?>
 				<table style="width: 300px">
 				<tr>
 					<td><label>Εγκεκριμένες παροχές</td>
-					<td><input type="radio" name="filter1" onClick="radio_click();" value="one" 
-					<?php if($choice=="111"||$choice=="112"||$choice=="121"||$choice=="122"){echo "checked=\"true\"";}?> /></label></td>
+					<td><input type="radio" name="filter1" onClick="radio_click();" value="one" <?php if (!$pending) {echo "checked=\"true\"";}?> /></label></td>
 					<td><label>Προς έγκριση παροχές</td>
-					<td><input type="radio" name="filter1" onClick="radio_click();" value="all"
-					<?php if($choice=="211"||$choice=="212"||$choice=="221"||$choice=="222"){echo "checked=\"true\"";}?> /></label></td>
+					<td><input type="radio" name="filter1" onClick="radio_click();" value="all" <?php if ($pending)  {echo "checked=\"true\"";}?> /></label></td>
 				</tr>
 				<tr>
-					<td><label>Τρέχοντος έτους</td>
-					<td><input type="radio" name="filter2" onClick="radio_click();" value="current"
-					<?php if($choice=="111"||$choice=="112"||$choice=="211"||$choice=="212"){echo "checked=\"true\"";}?> /></label></td>
-					<td><label>Παλιότερων ετών</td>
-					<td><input type="radio" name="filter2" onClick="radio_click();" value="old"
-					<?php if($choice=="121"||$choice=="122"||$choice=="221"||$choice=="222"){echo "checked=\"true\"";}?>/></label></td>
-				</tr>
-				<tr>
-					<td><label>Ενεργές παροχές</td>
-					<td><input type="radio" name="filter3" onClick="radio_click();" value="live"
-					<?php if($choice=="111"||$choice=="121"||$choice=="211"||$choice=="221"){echo "checked=\"true\"";}?> /></label></td>
-					<td><label>Ανενεργές παροχές</td>
-					<td><input type="radio" name="filter3" onClick="radio_click();" value="dead"
-					<?php if($choice=="112"||$choice=="122"||$choice=="212"||$choice=="222"){echo "checked=\"true\"";}?>/></label></td>
+					<td><label>Τρέχοuσες παροχές</td>
+					<td><input type="radio" name="filter2" onClick="radio_click();" value="current" <?php if ($current) {echo "checked=\"true\"";}?> /></label></td>
+					<td><label>Παλιές παροχές</td>
+					<td><input type="radio" name="filter2" onClick="radio_click();" value="old" <?php if (!$current) {echo "checked=\"true\"";}?>/></label></td>
 				</tr>
 				</table>
 				<br />	
@@ -320,23 +175,20 @@
 						{
 							extract($row);
 							echo "<tr>\n";
-							$ayear_row = get_ayear_from_academic_year_id($academic_year_id);
-							echo "<td>$id</td><td>$professor_name</td><td>$title</td><td>$candidates</td><td>$requirements</td><td>$deliverables</td><td>$hours</td><td>$deadline</td><td>$ayear_row[ayear]</td>";
-							echo "<td><input type='checkbox' disabled='true' " . (($winter_semester == 1) ? "checked='true'" : "") . ">";
-							echo "</td></tr>\n";
-						}			
+							echo "<td>$id</td><td>$professor_name</td><td>$title</td><td>$candidates</td><td>$requirements</td><td>$deliverables</td><td>$hours</td><td>$deadline</td><td>$start_date</td><td>$end_date</td>";
+							echo "</tr>\n";
+						}
 				?>	
 				</tbody>
 				</table>
 				
 				<br />
 				<p>
-				<?php if($choice=="211"){?><input class="button" type="submit" name="submit_btn" value="Έγκριση παροχής"  /><?php	}?>
+				<input class="button" type="submit" name="submit_btn" value="Έγκριση παροχής"  />
 				</p>
 				</div>
-			</form>		
+			</form>
 		</div>
-	<?php	}	?>
 	</aside>
 	</div><!--/content--> 
  
--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT'].'/includes/connection.php');
-    //This file is the place to store all basic functions
+
 	function confirm_query($result_set)
 	{
 		if (!$result_set)
@@ -28,66 +28,6 @@
 		$result_set = mysql_query($query, $con);
 		confirm_query($result_set);
 		return $result_set;
-	}
-
-
-	function get_admin_data($choice)
-	{
-	//To 111 simainei egkekrimenes-trexon etos-energes
-	//To 212 simainei oloi oi pros egkrisi-trexon etos-anenerges
-		global $con;
-		$row = get_current_year();//pernei to id tou current year
-		$current_year = $row['id'];
-
-		switch ($choice) {
-			case"211":
-				$query1 = "SELECT * FROM work_offers WHERE published = false AND academic_year_id = '$current_year' AND has_expired = false";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			case"221":
-				$query1 = "SELECT * FROM work_offers WHERE published = false AND academic_year_id <> '$current_year' AND has_expired = false";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			case"212":
-				$query1 = "SELECT * FROM work_offers WHERE published = false AND academic_year_id = '$current_year' AND has_expired = true";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			case"222":
-				$query1 = "SELECT * FROM work_offers WHERE published = false AND academic_year_id <> '$current_year' AND has_expired = true";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			case"112":
-				$query1 = "SELECT * FROM work_offers WHERE published = true AND academic_year_id = '$current_year' AND has_expired = true";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			case"121":
-				$query1 = "SELECT * FROM work_offers WHERE published = true AND academic_year_id <> '$current_year' AND has_expired = false";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			case"122":
-				$query1 = "SELECT * FROM work_offers WHERE published = true AND academic_year_id <> '$current_year' AND has_expired = true";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-			break;
-			default:
-				$query1 = "SELECT * FROM work_offers WHERE published = true AND academic_year_id = '$current_year' AND has_expired = false";
-				$result_set1 = mysql_query($query1,$con);
-				confirm_query($result_set1);
-				return $result_set1;
-		}
 	}
 
 ?>
